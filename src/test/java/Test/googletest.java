78b22477fd3dabee4db93b0efab8486dc596390e
gwalters <gwalters@hotmail.co.uk>
2018-02-25 00:08:36 +0000

    added first test

--- src/test/java/Test/googletest.java
+++ src/test/java/Test/googletest.java
@@ -1,13 +1,16 @@
 package Test;
-import com.codeborne.selenide.Selenide;
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class googletest {
 
-    //my first test
+    //my first test using power of selenide
     @Test
     public void usercanopengoogle() {
     open("http://google.com");
+    $(By.name("q")).setValue("testing").pressEnter();
     }
 }
